13926612077e00b19b8b82b7a6caf75bac7d714b
akssayy <waghralakshay@gmail.com>
2024-08-19 19:44:01 +0530

    ifelse

---
+++ ifelse.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+take thge age from user and then decide accordingly
+1.If age < 18, 
+print-> "not eligble for  job"
+2. If age >= 18,
+print->"eligble for job"
+3. If age >=55 and age <=57,
+print->"eligible for job, but retirement soon."
+4.If age >57 
+print-> "retirement time "
+*/
+
+int main() {
+    int age;
+    cin >> age;
+    if(age < 18){
+        cout << "not eligible for job";
+    }
+
+    else if(age >= 18){
+        cout << "eligible for job";
+    }
+
+    else if(age >=55 && age <=57){
+        cout << "eligible for job but retirment soon";
+    }
+
+    else if(age > 57){
+        cout << "retirement time" ;
+    }
+}
