9328dc499e68bbb789538a41978a252f9a00625b
akssayy <waghralakshay@gmail.com>
2024-08-19 18:24:03 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //int, long, long long, float, double
+    //string and getline
+    string s1;
+    string s2;
+    cin >> s1 >>s2;
+    cout << s1 <<" " << s2;
+    return 0;
+
+}
+
+
