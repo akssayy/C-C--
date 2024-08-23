ff05be0110e3cd85bee830d91285322fec794072
akssayy <waghralakshay@gmail.com>
2024-08-23 11:09:55 +0530

    Do while

---
+++ while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;  // Initialize the counter
+
+    // Do-while loop to print numbers from 1 to 10
+    do {
+        cout << i << " ";
+        i++;  // Increment the counter
+    } while (i <= 10);
+
+    // Add a new line at the end of the output
+    cout << endl;
+
+    return 0;
+}
