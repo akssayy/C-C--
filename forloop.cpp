8daee0be567421ba33e21b19c17ff5b65f7efa2d
akssayy <waghralakshay@gmail.com>
2024-08-23 11:07:13 +0530

    for loop

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // For loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    
+    // Add a new line at the end of the output
+    cout << endl;
+
+    return 0;
+}
