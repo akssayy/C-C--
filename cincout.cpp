c744f35b0d34c5e73ad11b86aafdaee2751d2487
akssayy <waghralakshay@gmail.com>
2024-08-30 00:35:52 +0530

    cincout

---
+++ cincout.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+ 
+    int a, b, c;
+    cin >> a >> b >> c;  // Read three space-separated integers
+    int sum = a + b + c; // Calculate the sum
+    cout << sum << endl; // Output the sum
+    return 0;
+
+}
