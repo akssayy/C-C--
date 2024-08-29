275a4b8a29236eb093527c7a2c19543213ebec82
akssayy <waghralakshay@gmail.com>
2024-08-30 00:09:07 +0530

    calculator

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x, y;
+    int sum;
+    cout << "enter a number:";
+    cin >> x;
+    cout <<"enter another number: ";
+    cin >> y;
+    sum = x + y ;
+    cout <<"sum is: " <<sum;
+    return 0;
+
+
+}
+
