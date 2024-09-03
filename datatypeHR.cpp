1550c1c69d0e348a4361008a75788320fedaaaee
akssayy <waghralakshay@gmail.com>
2024-09-03 10:10:28 +0530

    hackerank

---
+++ datatypeHR.cpp
@@ -0,0 +1,35 @@
+/*Input Format
+
+Input consists of the following space-separated values: int, long, char, float, and double, respectively */
+
+/*
+Output Format
+
+Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a ;
+    long b ;
+    char c ;
+    float d;
+    double e;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    return 0;
+}
